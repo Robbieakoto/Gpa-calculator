5e0331c11634c86c8c21b80dfbc5f665fa9ffbd3
Robbie <33154097+Robbieakoto@users.noreply.github.com>
2018-01-31 09:19:08 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My first  Javascript</title>
+    
+    <script type="text/javascript">
+    function Hello() {
+    alert("Hello world");
+    }
+    </script>
+    
+    <script type="text/javascript">
+    var baffour=24;
+    var roberta=22;
+    var saliha=21;
+    var michael=23;
+    var asare=22;
+    var emma=24;
+
+        var average=(baffour+roberta+saliha+michael+asare+emma)/6;
+        document.write("average");
+    </script><br><br><br>
+        
+
+</head>
+<body>
+         
+<input type="button" onclick="Hello();" name="ok" value="click me"/>
+</body>
+</html>
